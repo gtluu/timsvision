d9845117d298204a2805498f8bfee35122daaea9
claricep <clariceparky24@gmail.com>
2023-01-19 11:37:54 -0800

    sqlite

--- timsvision.py
+++ timsvision.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import time
+import sqlite3 as sq
 from dash import Dash, dcc, html, Input, Output, State, callback_context
 from pyimzml.ImzMLParser import ImzMLParser, getionimage
 
@@ -12,10 +14,11 @@ app.layout = html.Div([
         html.Img(src='assets/timsvision_logo_mini.png', alt='TIMSvision Logo', width="375")
     ], style={'display': 'flex', 'justifyContent': 'center', 'padding': '25px'}, className='row'),
 
+#shift down
     html.Div(children=[
         html.Div(children=[
             html.H4('Input imzML File:'),
-        ], style={'display': 'flex', 'justifyContent': 'center', 'font-family': 'Times New Roman', 'font-size': '22px', 'width': '1440px', 'height': '25px', 'position':'relative', 'top':'-35px'}),
+        ], style={'width': '250px', 'display': 'flex', 'justifyContent': 'center', 'font-family': 'Times New Roman', 'font-size': '20px', 'width': '1440px', 'height': '25px', 'position':'relative', 'top':'-35px'}),
         html.Div(children=[
             dcc.Input(id='path', placeholder='imzML File', type='text')
         ], style={'display': 'flex', 'justifyContent': 'center', 'width': '100%', 'padding' : '10px'})
@@ -55,7 +58,7 @@ app.layout = html.Div([
         ], style={'border': '1px solid black','display': 'inline-block', 'vertical-align': 'top', 'position':'relative', 'top':'-350px'})
     ], className='row'),
 
-#label table, shift second table
+#label table, center second table
     html.Div(children=[
         html.Div(children=[
             dcc.Graph(id='contour', figure={}),
@@ -83,24 +86,39 @@ def load_data(n_clicks, path, mass, mass_tol, ook0, ook0_tol):
         mz_array = np.zeros(0)
         intensity_array = np.zeros(0)
         mobility_array = np.zeros(0)
+        start = time.time()
         for i in range(0, len(data.coordinates)):
             mzs, ints, mobs = data.getspectrum(i)
-            mz_array = np.append(mz_array, mzs)
-            intensity_array = np.append(intensity_array, ints)
-            mobility_array = np.append(mobility_array, mobs)
+            mz_array = np.append(mz_array, mzs) #1
+            intensity_array = np.append(intensity_array, ints)  #2
+            mobility_array = np.append(mobility_array, mobs) #3
+        print('loop time flag: ' + str((time.time() - start)))
+
         data_df = pd.DataFrame(data={'mz': mz_array,
                                      'intensity': intensity_array,
                                      'mobility': mobility_array})
 
+        start = time.time()
         contour = data_df.groupby(['mz', 'mobility'], as_index=False).aggregate(sum)
         contour = contour[contour['intensity'] >= (np.max(contour['intensity']) * 0.0002)]
         ion_image = getionimage(data, mz_value=float(mass), mz_tol=float(mass_tol),
                                 mob_value=float(ook0), mob_tol=float(ook0_tol))
+        print('ion time flag: ' + str((time.time() - start)))
 
+        conn = sq.connect('{}.sqlite'.format('timsvisions_extension')) # creates file
+        data_df.to_sql('timsvisions_extension', conn, if_exists='replace', index=False) # writes to file
+        conn.close() # good practice: close connection
+
+        start = time.time()
         contour_plot = px.density_contour(data_frame=contour, x='mz', y='mobility',
                                           marginal_x='histogram', marginal_y='histogram', histfunc='sum',
                                           nbinsx=20000, nbinsy=len(set(contour['mobility'])) // 2)
+        print(contour_plot)
+        print('contour time flag: ' + str((time.time() - start)))
+
+        start = time.time()
         ion_image_plot = px.imshow(ion_image, color_continuous_scale='viridis')
+        print('ion time flag: ' + str((time.time() - start)))
 
         return [contour_plot, ion_image_plot, {'stored_path': path}]
 
@@ -112,7 +130,6 @@ def update_inputs(coords):
     mass = coords['points'][0]['x']
     ook0 = coords['points'][0]['y']
     return [mass, ook0]
-
 
 if __name__ == '__main__':
     app.run_server(debug=False, port=8051)
