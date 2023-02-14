b8367fd05e04bc2f62e46b790dd041e581a422fa
claricep <70664877+claricep@users.noreply.github.com>
2023-02-14 12:06:11 -0800

    undo to old code

--- timsvision.py
+++ timsvision.py
@@ -4,383 +4,145 @@ import pandas as pd
 import plotly.express as px
 import time
 import sqlite3 as sq
-import base64
-import dash_bootstrap_components as dbc
-
-from dash import Dash, dcc, html, State, callback_context
-from dash_extensions.enrich import Input, Output, DashProxy, MultiplexerTransform
+from dash import Dash, dcc, html, Input, Output, State, callback_context
 from pyimzml.ImzMLParser import ImzMLParser, getionimage
-#from dash_extensions.callback import CallbackCache
-#from flask_caching.backends import FileSystemCache
 
-start_time = time.time()
-# relative path for directory where uploaded data is stored
-DATA = None
-DF = None
-UPLOAD_DIR = 'upload'
-if not os.path.exists(UPLOAD_DIR):
-    os.makedirs(UPLOAD_DIR)
+app = Dash(__name__)
 
-# Use DashProxy instead of Dash to allow for multiple callbacks to the same plot
-app = DashProxy(prevent_initial_callbacks=True, transforms=[MultiplexerTransform()])
-#cc = CallbackCache(cache=FileSystemCache(cache_dir="cache"), instant_refresh=True)
-app.layout = html.Div(
-    [
-        # logo element
-        html.Div(
-            children=[
-                html.Img(
-                    src='assets/timsvision_logo_mini.png',
-                    alt='TIMSvision Logo',
-                    width="375"
-                )
-            ],
-            style={
-                'display': 'flex',
-                'justifyContent': 'center',
-                'padding': '25px'},
-            className='row'
-        ),
+app.layout = html.Div([
+    html.Div(children=[
+        html.Img(src='assets/timsvision_logo_mini.png', alt='TIMSvision Logo', width="375")
+    ], style={'display': 'flex', 'justifyContent': 'center', 'padding': '25px'}, className='row'),
 
-        # upload element
-        html.Div(
-            children=[
-                html.Div(
-                    children=[
-                        html.H4('Input imzML File:'),
-                    ],
-                    style={
-                        'width': '1440px',
-                        'height': '25px',
-                        'display': 'flex',
-                        'justifyContent': 'center',
-                        'font-family': 'Arial',
-                        'font-size': '20px',
-                        'position': 'relative',
-                        'top': '-35px'
-                    }
-                ),
-                html.Div(
-                    children=[
-                        dcc.Input(
-                            id='path',
-                            placeholder='imzML File',
-                            type='text'
-                        )
-                    ],
-                    style={
-                        'display': 'flex',
-                        'justifyContent': 'center',
-                        'width': '100%',
-                        'padding': '10px'
-                    }
-                ),
-                html.Div(
-                    children=[
-                        html.Button(
-                            'Load',
-                            id='load'
-                        )
-                    ],
-                    style={
-                        'display': 'flex',
-                        'justifyContent': 'center',
-                        'width': '100%',
-                        'padding': '10px'
-                    }
-                )
-            ],
-            className='row',
-        ),
+#shifit down
+    html.Div(children=[
+        html.Div(children=[
+            html.H4('Input imzML File:'),
+        ], style={'width': '250px', 'display': 'flex', 'justifyContent': 'center', 'font-family': 'Times New Roman', 'font-size': '20px', 'width': '1440px', 'height': '25px', 'position':'relative', 'top':'-35px'}),
+        html.Div(children=[
+            dcc.Input(id='path', placeholder='imzML File', type='text')
+        ], style={'display': 'flex', 'justifyContent': 'center', 'width': '100%', 'padding' : '10px'})
+    ], className='row'),
 
-        # ion image ui elements
-        html.Div(
-            id='ion_image_block',
-            className='row'
-        ),
+#blue border #0047AB
+    html.Div(children=[
+        html.Div(children=[
+            html.H5('m/z:'),
+        ], style={'display': 'inline-block', 'position':'relative', 'top':'90px','font-family': 'Times New Roman', 'font-size': '20px', 'padding-left': '120px'}),
+        html.Div(children=[
+            dcc.Input(id='mass', value=0, type='text'),
+        ], style={'position':'relative', 'top':'65px', 'width': '150px','font-family': 'Times New Roman', 'font-size': '20px', 'padding-left': '120px', 'border-color': '#0047AB'}),
+        html.Div(children=[
+            html.H5('m/z Tolerance:'),
+        ], style={'position':'relative', 'top':'60px', 'display': 'inline-block','font-family': 'Times New Roman', 'font-size': '20px', 'padding-left': '120px'}),
+        html.Div(children=[
+            dcc.Input(id='mass_tol', value=0, type='text'),
+        ], style={'position':'relative', 'top':'35px', 'width': '150px','font-family': 'Times New Roman', 'font-size': '20px', 'padding-left': '120px'}),
+        html.Div(children=[
+            html.H5('1/K0:'),
+        ], style={'position':'relative', 'top':'30px', 'display': 'inline-block','font-family': 'Times New Roman', 'font-size': '20px', 'padding-left': '120px'}),
+        html.Div(children=[
+            dcc.Input(id='ook0', value=0, type='text'),
+        ], style={'position':'relative', 'top':'5px', 'width': '150px','font-family': 'Times New Roman', 'font-size': '20px', 'padding-left': '120px'}),
+        html.Div(children=[
+            html.H5('1/K0 Tolerance:'),
+        ], style={'position':'relative', 'top':'0px', 'display': 'inline-block', 'font-family': 'Times New Roman', 'font-size': '20px', 'padding-left': '120px'}),
+        html.Div(children=[
+            dcc.Input(id='ook0_tol', value=0, type='text'),
+        ], style={'position':'relative', 'top':'-25px', 'width': '150px','font-family': 'Times New Roman', 'font-size': '20px', 'padding-left': '120px'}),
+        html.Div(children=[
+            html.Div(html.Button('Update Plots', id='update')),
+        ], style={'border-radius': '20px', 'display': 'inline-block', 'margin-right': '9vw', 'font-family': 'Times New Roman', 'font-size': '25px', 'position':'relative', 'top':'-10px', 'padding-left': '140px'}),
+        html.Div(children=[
+            dcc.Graph(id='image', figure={})
+        ], style={'border': '1px solid black','display': 'inline-block', 'vertical-align': 'top', 'position':'relative', 'top':'-350px'})
+    ], className='row'),
 
-        html.Div(
-            id='contour_block',
-            className='row'
-        ),
-    ],
-    style={
-        'font-family': 'Lucida Sans Unicode'
-    }
-)
+#label table, center second table
+    html.Div(children=[
+        html.Div(children=[
+            dcc.Graph(id='contour', figure={}),
+        ], style = {'border': '1px solid black', 'position':'relative', 'top':'-250px', 'width': '1250px', 'margin-right': '10vw'})
+    ], className='row'),
+
+    dcc.Store(id='stored_path')
+], style={'font-family': 'Lucida Sans Unicode'})
 
 
-def get_contour_plot(data_df):
-    print('contour start')
-    start = time.time()
-    contour_df = data_df.groupby(['mz', 'mobility'], as_index=False).aggregate(sum)
-    contour_df = contour_df[contour_df['intensity'] >= (np.max(contour_df['intensity']) * 0.0002)]
-    contour_df = contour_df.round({'mz': 4, 'mobility': 3})
-    print(contour_df)
-    print('contour df time flag: ' + str((time.time() - start)))
-    start = time.time()
-    contour_plot = px.density_contour(data_frame=contour_df, x='mz', y='mobility',
-                                      marginal_x='histogram', marginal_y='histogram', histfunc='sum',
-                                      nbinsx=20000, nbinsy=len(set(contour_df['mobility'])) // 2)
-    print('contour plot time flag: ' + str((time.time() - start)))
-
-    children = [
-       html.Div(
-           dcc.Graph(
-               id='contour',
-               figure=contour_plot
-           ),
-           style={
-               'border': '1px solid black',
-               'position': 'relative',
-               'top': '-250px',
-               'width': '1250px',
-               'margin-right': '10vw'
-           }
-       )
-    ]
-    return children
-
-
-def get_ion_image(data, mass=0, mass_tol=0, ook0=0, ook0_tol=0):
-    start = time.time()
-    ion_image = getionimage(data,
-                            mz_value=float(mass),
-                            mz_tol=float(mass_tol),
-                            mob_value=float(ook0),
-                            mob_tol=float(ook0_tol))
-
-    ion_image_plot = px.imshow(ion_image, color_continuous_scale='viridis')
-    print('get_ion_image time flag: ' + str((time.time() - start)))
-
-    children = [
-        html.Div(
-            children=[
-                html.H5('m/z:'),
-            ],
-            style={
-                'display': 'inline-block',
-                'position': 'relative',
-                'top': '90px',
-                'font-family': 'Arial',
-                'font-size': '20px',
-                'padding-left': '120px'
-            }
-        ),
-        html.Div(
-            children=[
-                dcc.Input(
-                    id='mass',
-                    value=mass,
-                    type='text'
-                ),
-            ],
-            style={'position': 'relative',
-                   'top': '65px',
-                   'width': '150px',
-                   'font-family': 'Arial',
-                   'font-size': '20px',
-                   'padding-left': '120px',
-                   'border-color': '#0047AB'
-                   }
-        ),
-        html.Div(
-            children=[
-                html.H5('m/z Tolerance:'),
-            ],
-            style={
-                'position': 'relative',
-                'top': '60px',
-                'display': 'inline-block',
-                'font-family': 'Arial',
-                'font-size': '20px',
-                'padding-left': '120px'
-            }
-        ),
-        html.Div(
-            children=[
-                dcc.Input(
-                    id='mass_tol',
-                    value=mass_tol,
-                    type='text'
-                ),
-            ],
-            style={'position': 'relative',
-                   'top': '35px',
-                   'width': '150px',
-                   'font-family': 'Arial',
-                   'font-size': '20px',
-                   'padding-left': '120px'}),
-        html.Div(
-            children=[
-                html.H5('1/K0:'),
-            ],
-            style={
-                'position': 'relative',
-                'top': '30px',
-                'display': 'inline-block',
-                'font-family': 'Arial',
-                'font-size': '20px',
-                'padding-left': '120px'
-            }
-        ),
-        html.Div(
-            children=[
-                dcc.Input(
-                    id='ook0',
-                    value=ook0,
-                    type='text'
-                ),
-            ],
-            style={
-                'position': 'relative',
-                'top': '5px',
-                'width': '150px',
-                'font-family': 'Arial',
-                'font-size': '20px',
-                'padding-left': '120px'
-            }
-        ),
-        html.Div(
-            children=[
-                html.H5('1/K0 Tolerance:'),
-            ],
-            style={
-                'position': 'relative',
-                'top': '0px',
-                'display': 'inline-block',
-                'font-family': 'Arial',
-                'font-size': '20px',
-                'padding-left': '120px'
-            }
-        ),
-        html.Div(
-            children=[
-                dcc.Input(
-                    id='ook0_tol',
-                    value=ook0_tol,
-                    type='text'
-                ),
-            ],
-            style={
-                'position': 'relative',
-                'top': '-25px',
-                'width': '150px',
-                'font-family': 'Arial',
-                'font-size': '20px',
-                'padding-left': '120px'
-            }
-        ),
-        html.Div(
-            children=[
-                html.Div(
-                    html.Button(
-                        'Update Ion Image',
-                        id='update'
-                    )
-                ),
-            ],
-            style={
-                'border-radius': '20px',
-                'display': 'inline-block',
-                'margin-right': '9vw',
-                'font-family': 'Arial',
-                'font-size': '25px',
-                'position': 'relative',
-                'top': '-10px',
-                'padding-left': '140px'
-            }
-        ),
-        html.Div(
-            id='ion_image',
-            children=[
-                dcc.Graph(
-                    id='image',
-                    figure=ion_image_plot
-                )
-            ],
-            style={
-                'border': '1px solid black',
-                'display': 'inline-block',
-                'vertical-align': 'top',
-                'position': 'relative',
-                'top': '-350px'
-            }
-        )
-    ]
-    return children
-
-@app.callback(Output('contour_block', 'children'),
-              Output('ion_image_block', 'children'),
-              Input('load', 'n_clicks'),
-              State('path', 'value'))
-def upload_data(n_clicks, path):
-    changed_id = [i['prop_id'] for i in callback_context.triggered][0]
-
-    if 'load' in changed_id:
-        if path.lower().endswith('imzml'):
-            global DATA
-            print('Parsing imzML File')
-            DATA = ImzMLParser(path, include_spectra_metadata='full', include_mobility=True)
-            print('Creating Master DataFrame')
-
-            rows = []
-            for i in range(0, len(DATA.coordinates)):
-                mzs, ints, mobs = DATA.getspectrum(i)
-                rows.append(pd.DataFrame({'mz': mzs,
-                                          'intensity': ints,
-                                          'mobility': mobs}))
-            global DF
-            DF = pd.concat(rows)
-            print('Getting Contour Plot')
-            contour_child = get_contour_plot(DF)
-            print('Getting Overall Ion Image')
-            ion_image_child = get_ion_image(DATA)
-            return [contour_child, ion_image_child]
-
-
-@app.callback(Output('ion_image_block', 'children'),
-              Input('update', 'n_clicks'),
-              State('mass', 'value'),
-              State('mass_tol', 'value'),
-              State('ook0', 'value'),
-              State('ook0_tol', 'value'))
-
-def update_ion_image(n_clicks, mass, mass_tol, ook0, ook0_tol):
+@app.callback(Output(component_id='contour', component_property='figure'),
+              Output(component_id='image', component_property='figure'),
+              Output(component_id='stored_path', component_property='data'),
+              Input(component_id='update', component_property='n_clicks'),
+              State(component_id='path', component_property='value'),
+              State(component_id='mass', component_property='value'),
+              State(component_id='mass_tol', component_property='value'),
+              State(component_id='ook0', component_property='value'),
+              State(component_id='ook0_tol', component_property='value'),)
+def load_data(n_clicks, path, mass, mass_tol, ook0, ook0_tol):
     changed_id = [i['prop_id'] for i in callback_context.triggered][0]
 
     if 'update' in changed_id:
-        global DATA
-        print('Updating Ion Image')
-        print("--- %s seconds ---" % (time.time() - start_time))
-        return get_ion_image(DATA,
-                             mass=mass,
-                             mass_tol=mass_tol,
-                             ook0=ook0,
-                             ook0_tol=ook0_tol)
+        start = time.time()
+        data = ImzMLParser(path, include_spectra_metadata='full', include_mobility=True)
+        print('upload time flag: ' + str((time.time() - start)))
+        mz_array = np.zeros(0)
+        intensity_array = np.zeros(0)
+        mobility_array = np.zeros(0)
+
+        start = time.time()
+        data = ImzMLParser(path, include_spectra_metadata='full', include_mobility=True)
+        print('loop time flag: ' + str((time.time() - start)))
+        current = time.time()
+        mz_array = np.zeros(0)
+        intensity_array = np.zeros(0)
+        mobility_array = np.zeros(0)
+        for i in range(0, len(data.coordinates)):
+            startLoopTime = time.time()
+            mzs, ints, mobs = data.getspectrum(i)
+            mz_array = np.append(mz_array, mzs) #1
+            intensity_array = np.append(intensity_array, ints)  #2
+            mobility_array = np.append(mobility_array, mobs) #3
+        print('loop time flag: ' + str((time.time() - start)))
+
+        data_df = pd.DataFrame(data={'mz': mz_array,
+                                     'intensity': intensity_array,
+                                     'mobility': mobility_array})
+
+        start = time.time()
+        contour = data_df.groupby(['mz', 'mobility'], as_index=False).aggregate(sum)
+        contour = contour[contour['intensity'] >= (np.max(contour['intensity']) * 0.0002)]
+
+        ion_image = getionimage(data, mz_value=float(mass), mz_tol=float(mass_tol),
+                                mob_value=float(ook0), mob_tol=float(ook0_tol))
+        print('ion time flag: ' + str((time.time() - start)))
+
+        conn = sq.connect('{}.sqlite'.format('timsvisions_extension'))
+        data_df.to_sql('timsvisions_extension', conn, if_exists='replace', index=False) 
+        conn.close() 
+
+        ion_image = getionimage(data, mz_value=float(mass), mz_tol=float(mass_tol), mob_value=float(ook0), mob_tol=float(ook0_tol))
+
+        start = time.time()
+        contour_plot = px.density_contour(data_frame=contour, x='mz', y='mobility',
+                                          marginal_x='histogram', marginal_y='histogram', histfunc='sum',
+                                          nbinsx=20000, nbinsy=len(set(contour['mobility'])) // 2)
+        print('contour time flag: ' + str((time.time() - start)))
+
+        start = time.time()
+        ion_image_plot = px.imshow(ion_image, color_continuous_scale='viridis')
+        print('ion time flag: ' + str((time.time() - start)))
+
+        return [contour_plot, ion_image_plot, {'stored_path': path}]
 
 
-@app.callback(Output('mass', 'value'),
-              Output('ook0', 'value'),
+@app.callback(Output(component_id='mass', component_property='value'),
+              Output(component_id='ook0', component_property='value'),
               Input('contour', 'clickData'),)
 
-# def update_inputs(coords):
-#     print('Updated Coordinates')
-#     mass = ['points', 0, 'x']
-#     print(mass)
-#     ook0 = ['points',0,'y']
-#     print(ook0)
-#     return [mass, ook0]
-
-def update_inputs(coords, mass_tol, ook0_tol):
-    print('Updating Ion Image from Heatmap')
+def update_inputs(coords):
     mass = coords['points'][0]['x']
     ook0 = coords['points'][0]['y']
-    return get_ion_image(DATA,
-                         mass=mass,
-                         mass_tol=mass_tol,
-                         ook0=ook0,
-                         ook0_tol=ook0_tol)
+    return [mass, ook0]
 
 if __name__ == '__main__':
-    app.run_server(debug=False, port=8050)
+    app.run_server(debug=False, port=8051)
